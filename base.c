f031e16cf283570aa7e485d46a7d66cbcf13b214
Gustavo Sousa <gfsousa42@gmail.com>
2023-05-18 16:33:18 +0000

    withcomments

--- base.c
+++ base.c
@@ -2,63 +2,87 @@
 #include <sys/wait.h>
 #include <string.h>
 
+// Função para imprimir erros
 void print_error(char *str)
 {
     while (*str)
         write(2, str++, 1);
 }
 
+// Função para lidar com o comando "cd"
 int cd(char **argv, int i)
 {
+    // Verifica se o número de argumentos é diferente de 2
     if (i != 2)
-        return (print_error("error: cd: bad arguments"), 1);
+        return (print_error("error: cd: bad arguments"), 1); // Imprime erro e retorna 1
+    
+    // Tenta mudar para o diretório especificado em argv[1]
     else if (chdir(argv[1]) == -1)
         return (print_error("bla bla"), 1);
+    
     return 0;
 }
 
+// Função para executar comandos externos
 int exec(char **argv, char **envp, int i)
 {
     int status;
     int fds[2];
-    int has_pipe = (argv[i] && !strcmp(argv[i], "|"));
+    int has_pipe = (argv[i] && !strcmp(argv[i], "|")); // Verifica se há um pipe no comando
+
+    // Verifica se há pipe e cria o pipe
     if (has_pipe && pipe(fds) == -1)
         return (print_error("pipe error"), 1);
+    
     int pid = fork();
-    if (!pid)
+
+    if (pid = == 0) // Se o pid for 0, é o processo filho
     {
-        argv[i] = 0;
+        argv[i] = 0; // Define o último argumento como NULL para indicar o fim dos argumentos
+        
         if (has_pipe && (dup2(fds[1], 1) == -1 || close(fds[0]) == -1 || close(fds[1]) == -1))
-            return (print_error("pipe setup error"), 1);
+            return (print_error("pipe setup error"), 1)
+        
         execve(*argv, argv, envp);
-        return (print_error("execve error"), 1);
+        
+        return (print_error("execve error"), 1); // Imprime erro e retorna 1 se execve falhar
     }
+
     waitpid(pid, &status, 0);
+
     if (has_pipe && (dup2(fds[0], 0) == -1 || close(fds[0]) == -1 || close(fds[1]) == -1))
         return (print_error("pipe cleanup error"), 1);
-    return WIFEXITED(status) && WEXITSTATUS(status);
+    
+    return WIFEXITED(status) && WEXITSTATUS(status); // Retorna o status de saída do processo filho
 }
 
 int main(int argc, char **argv, char **envp)
 {
     int status = 0;
     int i;
+
     if (argc > 1)
     {
-        argv++;
+        argv++; // Incrementa o ponteiro para ignorar o nome do programa
+        
         while (*argv && *(argv + 1))
         {
             argv++;
             i = 0;
+
+            // Procura pelo próximo ponto-e-vírgula ou pipe no comando
             while (argv[i] && strcmp(argv[i], ";") && strcmp(argv[i], "|"))
                 i++;
-            if (!strcmp(*argv, "cd"))
+
+            if (!strcmp(*argv, "cd")) // executar um comando.
                 status = cd(argv, i);
-            else if (i)
-                status = exec(argv, envp, i);
-            argv += i;
+            else if (i) // Se houver argumentos no comando
+                status = exec(argv, envp, i); // Executa o comando externo
+
+            argv += i; // Incrementa o ponteiro para pular para o próximo comando
         }
     }
+
     return status;
 }
 
--- microshell.c
+++ microshell.c
@@ -1,21 +1,25 @@
-#include "microshell.h"
+#include "microshell.h" // Inclusão do arquivo de cabeçalho "microshell.h"
 #include <stdio.h>
 
-void	print_error(char *str)
+// Função para imprimir erros
+void print_error(char *str)
 {
 	while (*str)
 		write(2, str++, 1);
 }
 
-int	ft_cd(char **argv, int i)
+// Função para lidar com o comando "cd"
+int ft_cd(char **argv, int i)
 {
-	int	nxt;
+	int nxt;
 
 	nxt = i;
 	while (argv[nxt] != NULL && (strcmp(argv[nxt], ";") != 0) && (strcmp(argv[nxt], "|") != 0))
 	{
 		nxt++;
 	}
+
+	// Verifica se o número de argumentos é diferente de 2
 	if (nxt - i != 2)
 	{
 		print_error("error: cd: bad arguments\n");
@@ -28,10 +32,12 @@ int	ft_cd(char **argv, int i)
 		print_error("\n");
 		exit (1);
 	}
-	return (nxt - 1);
+
+	return (nxt - 1); // Retorna o índice para o próximo comando
 }
 
-int	question_has_pipe(char **argv, int i)
+// Verifica se o comando atual possui pipe ("|")
+int question_has_pipe(char **argv, int i)
 {
 	int di;
 
@@ -39,56 +45,72 @@ int	question_has_pipe(char **argv, int i)
 	while (argv[di] && strcmp(argv[di], ";") != 0)
 	{
 		if (!strcmp(argv[di], "|"))
-			return (1);
+			return (1); // Retorna 1 se encontrar um pipe
 		di++;
 	}
-	return 0;
+
+	return 0; // Retorna 0 se não encontrar um pipe
 }
 
-int	ft_exe(char **argv, int i, char **envp)
+// Função para executar comandos externos
+int ft_exe(char **argv, int i, char **envp)
 {
-	int	fildes[2] ;
-	int	has_pipe;
-	int	pid;
-	int	status;
+	int fildes[2];
+	int has_pipe;
+	int pid;
+	int status;
 
 	has_pipe = question_has_pipe(argv, i);
+
+	// Se houver pipe, cria um pipe usando "pipe"
 	if (has_pipe == 1)
 		pipe(fildes);
-	pid = fork();
-	if (pid == 0)
+
+	pid = fork(); // Cria um novo processo
+
+	if (pid == 0) // Se o pid for 0, é o processo filho
 	{
 		if (has_pipe && (dup2(fildes[1], 1) == -1 || close(fildes[0]) == -1 || close(fildes[1] == -1)))
 			exit (1);
-		execve(argv[i], &argv[i], envp);
+
+		execve(argv[i], &argv[i], envp); // Executa o comando externo
+
 		exit (1);
 	}
-	waitpid(pid, &status, 0);
+
+	waitpid(pid, &status, 0); // Espera pelo processo filho
+
 	if (has_pipe && (dup2(fildes[0], 0) == -1 || close(fildes[0]) == -1 || close(fildes[1] == -1)))
 		exit (1);
+
 	while (argv[i] != NULL && (strcmp(argv[i], ";") != 0) && (strcmp(argv[i], "|") != 0))
 		i++;
-	return (i);
 
+	return (i); // Retorna o índice para o próximo comando
 }
 
-int	main (int argc, char **argv, char **envp)
+int main (int argc, char **argv, char **envp)
 {
-	int	i;
+	int i;
 
 	if (argc == 1)
 		return (0);
+
 	i = 0;
+
 	while (argv[i] && argv[i + 1])
 	{
 		i++;
+
 		if (strcmp(argv[i], ";") != 0)
 		{
 			if (!strcmp(argv[i], "cd"))
-				i += ft_cd(argv, i);
+				i += ft_cd(argv, i); // Executa o comando "cd"
 			else
-				i = ft_exe(argv, i, envp);
+				i = ft_exe(argv, i, envp); // Executa um comando externo
 		}
 	}
+
 	return (0);
 }
+
